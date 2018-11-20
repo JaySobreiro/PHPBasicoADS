4d5a4630d20662ab5014f5a7e09282da833e040e
Professor <Aluno@DOSO121732.educacional.up>
2018-11-20 08:56:44 -0200

    Paginas iniciais

---
+++ aulas/aula14/agenda.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<title>Aula 14 - Minha Agenda</title>
+</head>
+<body class="container">
+
+	<?php include 'menu.php'; ?>
+
+	<h3 class="text-primary">Minha Agenda</h3>
+
+</body>
+</html>
---
+++ aulas/aula14/menu.php
@@ -0,0 +1,19 @@
+<br>
+<ul class="nav">
+	<li class="nav-item">
+		<a href="index.php"  class="nav-link">Home</a>
+	</li>
+	<li class="nav-item">
+		<a href="agenda.php" class="nav-link">Minha Agenda</a>
+	</li>
+	<li class="nav-item">
+		<a href="cadastro.php" class="nav-link">Cadastre-se</a>
+	</li>
+	<li class="nav-item">
+		<a href="login.php" class="nav-link">Entrar</a>
+	</li>
+	<li class="nav-item">
+		<a href="logout.php" class="nav-link">Sair</a>
+	</li>
+</ul>
+<br>
